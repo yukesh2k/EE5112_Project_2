75d90038402748e81df56db39733ae6ed4e84411
1028Luo <jiexingluo@Outlook.com>
2024-11-19 18:07:55 +0800

    finished task 5

--- Code/stompTrajCost.m
+++ Code/stompTrajCost.m
@@ -1,3 +1,5 @@
+% task 5 submission
+
 % Given a trajectory, calculate its cost
 function [Stheta, Qtheta] = stompTrajCost(robot_struct, theta,  R, voxel_world)
 % Compute the local trajectory cost at each discretization theta point, as 
@@ -24,7 +26,7 @@ for i = 2 : nDiscretize
     sphere_centers_prev = sphere_centers;
     % Calculate the kinematics of the manipulator, given the
     % configuration theta values at different time (i=2:nDiscretize)
-    [X, ~] = updateJointsWorldPosition(robot_struct, theta(:, i));
+    [X, T] = updateJointsWorldPosition(robot_struct, theta(:, i));
     [sphere_centers, radi] = stompRobotSphere(X);
     % xb: 3D workspace position of sphere b at the current time
     % Approximate the speed (xb_dot) using the finite difference of the current and
@@ -33,14 +35,20 @@ for i = 2 : nDiscretize
     qo_cost(i) = stompObstacleCost(sphere_centers,radi, voxel_world, vel);
     
     %% TODO: Define your qc_cost to add constraint on the end-effector
-    qc_cost = 0;
+    T_end_effector = T{8};
+    R_end_effector = T_end_effector(1:3, 1:3);
+    y_axis = R_end_effector(:, 2);
+    diff = y_axis - [0; 0; 1]; % diff between y axis and world z axis
+    qc_cost(i) = norm(diff, 1);
 end
 
 %% Local trajectory cost: you need to specify the relative weights between different costs
-Stheta = 1000*qo_cost + qc_cost;
+qc_coeff = 1000;
+Stheta = 1000*qo_cost + qc_coeff*qc_cost;
 
 % sum over time and add the smoothness cost
 theta = theta(:, 2:end-1);
+
 Qtheta = sum(Stheta) + 1/2 * sum(theta * R * theta', "all");
 
 end
--- Code/task5.m
+++ Code/task5.m
@@ -1,16 +1,18 @@
-% EE5112 task2 submission
-% robot_name = 'kinovaJacoJ2N7S300';
-robot_name = 'kinovaJacoJ2N7S300';
+% EE5112 task5 submission
+% robot_name = 'kinovaJacoJ2N6S300';
+robot_name = 'kinovaJacoJ2N6S300';
 robot = loadrobot(robot_name, 'DataFormat', 'column'); % Note there are different data formats for loading the robot (chosen in the 2nd argument)
-disp(robot.BodyNames);
+% disp(robot.BodyNames);
 
 numJoints = numel(homeConfiguration(robot));
 
 % endEffector = "EndEffector_Link";
-endEffector = "j2n7s300_end_effector";
+endEffector = "j2n6s300_end_effector";
 
 % Initial end-effector pose
-taskInit = trvec2tform([[0.4 0 0.2]])*axang2tform([0 1 0 pi]);
+% task5: new intial pose
+% taskInit = trvec2tform([[0.4 0 0.2]])*axang2tform([0 1 0 pi]);
+taskInit = trvec2tform([[0.4 0 0.2]])*axang2tform([0 1 0 -pi/2]);
 
 % Compute current robot joint configuration using inverse kinematics
 ik = inverseKinematics('RigidBodyTree', robot);
@@ -27,7 +29,9 @@ currentRobotJConfig = ik(endEffector, taskInit, weights, robot.homeConfiguration
 currentRobotJConfig = wrapToPi(currentRobotJConfig);
 
 % Final (desired) end-effector pose
-taskFinal = trvec2tform([0.35 0.55 0.35])*axang2tform([0 1 0 pi]);  
+% taskFinal = trvec2tform([0.2 0.55 0.35])*axang2tform([0 1 0 pi]); 
+% task5: new final pose with upright y axis 
+taskFinal = trvec2tform([-0.2 0.4 0.3])*axang2tform([0 1 0 -pi/2]);
 anglesFinal = rotm2eul(taskFinal(1:3,1:3),'XYZ');
 poseFinal = [taskFinal(1:3,4);anglesFinal']; % 6x1 vector for final pose: [x, y, z, phi, theta, psi]
 
